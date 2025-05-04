a1009a1167c9ee5889cb9a143405408088c60fb7
DuncanBC <dhbennettconner@gmail.com>
2025-05-04 00:05:27 -0400

    Add files via upload

--- Administrator.java
+++ Administrator.java
@@ -1,5 +1,9 @@
 public class Administrator extends Employee {
 
+    public Administrator() {
+        this.isAdmin = true;
+    }
+
     public void searchEmployee(int empID, String fname, String lname, Date dob, String ssn) {
 
     }
---
+++ EmployeeManagementSystem.java
@@ -0,0 +1,47 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class EmployeeManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("\n\tEmployee Management System Login");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Username: ");
+        String username = input.nextLine();
+        System.out.println("Enter Password: ");
+        String password = input.nextLine();
+        try {
+            db_conn myConn = new db_conn();
+            String sqlcommand1 = "SELECT empid, username, dob, email, HireDate, Salary, password, isAdmin "
+                    + "FROM employees " +
+                    "WHERE username = '" + username + "';";
+            Statement myStmt = myConn.connection.createStatement();
+            ResultSet myRS1 = myStmt.executeQuery(sqlcommand1);
+            while (myRS1.next()) {
+                String userPassword = myRS1.getString("password");
+                Boolean isAdmin = myRS1.getBoolean("isAdmin");
+                int empID = myRS1.getInt("empid");
+                if (password.equals(userPassword)) {
+                    if (isAdmin) {
+                        Administrator user = new Administrator();
+                        user.empID = empID;
+                        user.setEmployeeValues();
+                        user.mainMenu();
+                    } else {
+                        GeneralEmployee user = new GeneralEmployee();
+                        user.empID = empID;
+                        user.setEmployeeValues();
+                        user.mainMenu();
+                    }
+
+                }
+            }
+            myConn.connection.close();
+        } catch (Exception e) {
+            System.out.println("Error in main " + e.getLocalizedMessage());
+        }
+
+        input.close();
+
+    }
+}
